292ad259adbda3e0b3493deaf0e8ea7f13fd40f3
Kevin Klose <kevin.klose@teamdynamix.com>
2024-02-13 13:52:52 -0500

    first commit

---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,45 @@
+﻿using MonkeyFinder.Services;
+using MonkeyFinder.View;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+  public MonkeysViewModel()
+  {
+    Items = new ObservableCollection<Item>();
+    for (int i = 0; i < 100; i++)
+    {
+      Items.Add(new Item
+      {
+        Name = $"item {i}",
+        View = new Label
+        {
+          Text = "Content View Label"
+        }
+      });
+    }
+  }
+
+  public ObservableCollection<Item> Items { get; set; }
+
+  //[ObservableProperty]
+  //private bool _collectionIsVisible = true;
+
+  //[ObservableProperty]
+  //private bool _otherIsVisible;
+
+  //[RelayCommand]
+  //void MakeVisible()
+  //{
+  //  CollectionIsVisible = !CollectionIsVisible;
+  //  OtherIsVisible = !OtherIsVisible;
+  //}
+}
+
+public class Item
+{
+  public string Name { get; set; }
+
+  public IView View { get; set; }
+}
